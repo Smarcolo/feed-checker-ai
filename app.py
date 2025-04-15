00b0212d9eadc4992831ace9eaf464125e4bdafd
Smarcolo <marco@langolocalzature.com>
2025-04-15 10:12:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,10 @@ def search():
     
     return jsonify(filtered[:20])
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
